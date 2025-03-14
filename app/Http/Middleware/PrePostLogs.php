c32905fa23002367e1deeda631576fc89f0c9a4a
jhon <jhondennivistal@gmail.com>
2025-03-14 12:54:14 +0800

    Middleware: PrePostLogs added

---
+++ app/Http/Middleware/PrePostLogs.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Log;
+
+class PrePostLogs {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response {
+        $this->preRequestLogs($request);
+
+        $response = $next($request);
+
+        $this->postRequestLogs($response, $request);
+
+        return $response;
+    }
+
+    private function preRequestLogs(Request $request): void {
+        Log::info("<-----------------------------------[Pre Controller Process]----------------------------------->");
+        Log::info("Request URL: " . $request->fullUrl());
+        Log::info("Request Method: " . $request->method());
+        Log::debug("Request data: " . json_encode($request->all(), JSON_PRETTY_PRINT) );
+    }
+
+    private function postRequestLogs(Response $response, Request $request): void {
+        $status = $response->getStatusCode();
+        Log::info("Request URL: " . $request->fullUrl());
+        Log::info("Response Status: " . $status);
+        if($status > 199 && $status < 299) {
+            Log::debug("Response data: " . $response->getContent());
+        }
+        Log::info("<-----------------------------------[Post Controller Process]----------------------------------->");
+    }
+
+}
