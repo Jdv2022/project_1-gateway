78db281d12266949758233c6bc0bfbae4ad5213f
jhon <jhondennivistal@gmail.com>
2025-06-23 21:36:33 +0800

    UP

--- tests/Feature/FEUtilityControllerTest.php
+++ tests/Feature/FEUtilityControllerTest.php
@@ -5,10 +5,23 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Log;
 use DateTime;
+use Illuminate\Support\Facades\Redis;
 
 class FEUtilityControllerTest extends FeatureBaseClassTest {
 
     public function test_day_today_returns_formatted_datetime() {
+
+		$userId = 1;
+		$redisKey = 'user_' . $userId;
+
+        $userDataArray = json_decode(file_get_contents(base_path('tests/Fixtures/user.json')), true);
+        $userJson = json_encode($userDataArray);
+
+        Redis::shouldReceive('get')
+            ->once()
+            ->with($redisKey)
+            ->andReturn($userJson);
+		
         $timezone = 'Asia/Manila';
 
         $response = $this->postRequest('api/web/private/user/attendance/day/today', [
@@ -23,6 +36,17 @@ class FEUtilityControllerTest extends FeatureBaseClassTest {
     }
 
     public function test_day_today_requires_timezone() {
+		$userId = 1;
+		$redisKey = 'user_' . $userId;
+
+        $userDataArray = json_decode(file_get_contents(base_path('tests/Fixtures/user.json')), true);
+        $userJson = json_encode($userDataArray);
+
+        Redis::shouldReceive('get')
+            ->once()
+            ->with($redisKey)
+            ->andReturn($userJson);
+
         $response = $this->postRequest('api/web/private/user/attendance/day/today', []);
 		$testObjectResponse = $response['testObjectResponse'];
 		$payload = $response['payload'];
